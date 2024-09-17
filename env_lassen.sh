a283108cbfdee57333d47bcfd726cd4bbe2dde81
walkloud <awalker-loud@lbl.gov>
2024-09-16 17:01:35 -0700

    updating Lassen env

--- env_lassen.sh
+++ env_lassen.sh
@@ -1,46 +1,29 @@
-module load cuda/12.0.0
-module load cmake/3.21.1
-module load gcc/12.2.1
+#!/usr/bin/bash
+if [[ ! -z $1 ]]; then
+    if [ "$1" = "CPU" ]; then
+        EXT=$1
+    else
+        echo "unrecognized EXT = $EXT [CPU] - exiting"
+        exit
+    fi
+else
+    EXT="GPU"
+fi
+echo "EXT = $EXT"
+
+if [ "$EXT" = "CPU" ]; then
+    module load cmake/3.21.1
+    module load gcc/12.2.1
+elif [ "$EXT" = "GPU" ]; then
+    module load cuda/12.0.0
+    module load cmake/3.21.1
+    module load gcc/12.2.1
+fi
 module load ninja/1.9.0
+module list
 
-HDF5=hdf5-1.10.9
-CXX=mpicxx
-CC=mpicc
-CHROMA_SRC=chroma
-CHROMA=chroma_gpu
-#CHROMA_CPU=chroma_cpu
-
-QUDA_SRC=quda
-QUDA=quda
-export QUDA_GPU_ARCH=sm_70
-export QUDA_TARGET_TYPE="CUDA"
-
-QMP=qmp
-QDP_SRC=qdpxx
-QDP=qdpxx
-LALIBE_SRC=lalibe
-#LALIBE_CPU=lalibe_cpu
-LALIBE=lalibe_gpu
-
-CC=mpicc
-CXX=mpicxx
-
-HDF5=hdf5-1.10.9
-H5_ONOFF=ON
-
-#echo "Stack ${1}"
-#if [ "$1" = "H5" ]; then
-#  H5=ON
-#  CHROMA=chroma_H5
-#  QDP=qdpxx_H5
-#  QUDA=quda_H5
-#  LALIBE_CPU=lalibe_H5
-#  LALIBE=lalibe_gpu_H5
-   
-#fi
-#echo "Using/Setting up stack: ${CHROMA} ${QDP} ${QMP} ${QUDA}  Srcs: ${CHROMA_SRC} ${QUDA_SRC}"
-
-export TOPDIR_HIP=/usr/workspace/coldqcd/software/lassen_smpi_RR_hm/software/lassen
+# DIRECTORIES
+export TOPDIR_HIP=/usr/workspace/coldqcd/software/lassen_smpi_RR_hm/software/compile_chroma_etc
 export PERMFIX=${TOPDIR_HIP}/./coldqcd_permissions.sh
 export SRCROOT=${TOPDIR_HIP}/../source
 export BUILDROOT=${TOPDIR_HIP}/../build
@@ -50,13 +33,46 @@ export INSTALLROOT=${TOPDIR_HIP}/../install
 [[ -d $BUILDROOT ]] || mkdir -p $BUILDROOT
 [[ -d $SRCROOT ]] || mkdir -p $SRCROOT
 
-export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${INSTALLROOT}/${QMP}/lib:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${INSTALLROOT}/${HDF5}/lib:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${INSTALLROOT}/${QDP}/lib:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${INSTALLROOT}/${QUDA}/lib:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${INSTALLROOT}/${CHROMA}/lib:${LD_LIBRARY_PATH}
+# COMPILE ENV
+CXX=mpicxx
+CC=mpicc
+C_FLAGS="-fopenmp -O3 -std=gnu99 -mcpu=power9 -mtune=power9"
+CXX_FLAGS="-fopenmp -O3 -std=c++11 -mcpu=power9 -mtune=power9"
+export CMAKE=cmake
+export CMAKE_EXTRA_FLAGS="-DCMAKE_EXPORT_COMPILE_COMMANDS=1"
+export CMAKE_MAKE_FLAGS="--parallel $(nproc)"
 
-#export CUDA_HOME=$CUDA_DIR
 export CUDA_DIR=$CUDA_HOME
 export MPI_HOME=$MPI_ROOT
+
+export QUDA_GPU_ARCH=sm_70
+export QUDA_TARGET_TYPE="CUDA"
+
+# SRC
+QMP=qmp
+HDF5=hdf5-1.10.9
+QDP=qdpxx
+QUDA=quda
+CHROMA=chroma
+LALIBE=lalibe
+# branch
+QMP_BRANCH="--branch devel"
+QDP_BRANCH="--branch devel"
+QUDA_BRANCH="--branch feature/print-mdwf-b5c5"
+CHROMA_BRANCH="--branch feature/mdwf-quda-upgrade"
+LALIBE_BRANCH="--branch qedm"
+
+# BUILD/INSTALL EXT
+# USE $EXT in build scripts
+
+# LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${INSTALLROOT}/${QMP}_${EXT}/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${INSTALLROOT}/${HDF5}_${EXT}/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${INSTALLROOT}/${QDP}_${EXT}/lib:${LD_LIBRARY_PATH}
+if [ "$EXT" = "GPU" ]; then
+    export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
+    export LD_LIBRARY_PATH=${INSTALLROOT}/${QUDA}/lib:${LD_LIBRARY_PATH}
+fi
+export LD_LIBRARY_PATH=${INSTALLROOT}/${CHROMA}_${EXT}/lib:${LD_LIBRARY_PATH}
+
